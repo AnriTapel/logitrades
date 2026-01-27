969d1b6366036b01a565e3a90de45eaaa550fdfe
anri.tapel <anri.snep@gmail.com>
2026-01-27 21:39:25 +0200

    auto width for common modal

--- frontend/src/lib/components/ui/dialog/common-dialog.svelte
+++ frontend/src/lib/components/ui/dialog/common-dialog.svelte
@@ -48,7 +48,7 @@
 </script>
 
 <Dialog.Root {open} onOpenChange={handleOpenChange}>
-	<Dialog.Content class={cn('min-w-[300px] max-w-full', className)}>
+	<Dialog.Content class={cn('min-w-[300px] max-w-full w-auto', className)}>
 		{#if title}
 			<h2 class="text-lg font-semibold mb-1">{title}</h2>
 		{/if}
